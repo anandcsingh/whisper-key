3ce8f76463d8930b3a72dcc30b0359cdd3d24a89
anandcsingh <anandcsingh@gmail.com>
2023-10-17 08:49:59 -0400

    encryption testing

---
+++ contracts/src/tests/Encryption.test.ts
@@ -0,0 +1,67 @@
+import {
+  Field,
+  Mina,
+  PrivateKey,
+  PublicKey,
+  AccountUpdate,
+  CircuitString,
+  Encryption,
+  Encoding,
+} from 'o1js';
+
+/*
+ * This file specifies how to test the `Add` example smart contract. It is safe to delete this file and replace
+ * with your own tests.
+ *
+ * See https://docs.minaprotocol.com/zkapps for more info.
+ */
+
+let proofsEnabled = false;
+
+describe('Encrypt', () => {
+  let deployerAccount: PublicKey,
+    deployerKey: PrivateKey,
+    senderAccount: PublicKey,
+    senderKey: PrivateKey,
+    zkAppAddress: PublicKey,
+    zkAppPrivateKey: PrivateKey;
+
+  beforeEach(() => {
+    const Local = Mina.LocalBlockchain({ proofsEnabled });
+    Mina.setActiveInstance(Local);
+    ({ privateKey: deployerKey, publicKey: deployerAccount } =
+      Local.testAccounts[0]);
+    ({ privateKey: senderKey, publicKey: senderAccount } =
+      Local.testAccounts[1]);
+  });
+
+  it('can encrypt json data', async () => {
+    let credential = {
+      name: 'John Doe',
+      email: 'john.doe@whisper-key.com',
+      phone: '555-555-5555',
+      address: '123 Main St, Anytown, USA',
+      ssn: '123-45-6789',
+      dob: '01/01/1970',
+    };
+    let credentialString = CircuitString.fromString(credential.email);
+    let credentialField = credentialString.toFields();
+    let fields = Encryption.encrypt(credentialField, senderAccount);
+    console.log(fields.cipherText);
+    expect(fields.cipherText.length).toBeGreaterThan(0);
+  });
+
+  it('can encrypt and decrypt json data', async () => {
+    let message = 'This is a secret.';
+    let messageFields = Encoding.stringToFields(message);
+
+    // encrypt
+    let cipherText = Encryption.encrypt(messageFields, senderAccount);
+
+    // decrypt
+    let decryptedFields = Encryption.decrypt(cipherText, senderKey);
+    let decryptedMessage = Encoding.stringFromFields(decryptedFields);
+
+    expect(decryptedMessage).toEqual(message);
+  });
+});
