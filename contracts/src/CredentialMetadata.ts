a76bfc0b26d442507dbdd819a6271ee03ce55021
anandcsingh <anandcsingh@gmail.com>
2023-10-21 13:14:25 -0400

    adding credential repo

---
+++ contracts/src/CredentialMetadata.ts
@@ -0,0 +1,27 @@
+export class CredentialMetadata {
+  name: string;
+  description: string;
+  version: string;
+  created: Date;
+  createdBy: string;
+  fileName: string;
+  fileHash: string;
+
+  constructor(
+    name: string,
+    description: string,
+    version: string,
+    created: Date,
+    createdBy: string,
+    fileName: string,
+    fileHash: string
+  ) {
+    this.name = name;
+    this.description = description;
+    this.version = version;
+    this.created = created;
+    this.createdBy = createdBy;
+    this.fileName = fileName;
+    this.fileHash = fileHash;
+  }
+}
---
+++ contracts/src/tests/CredentialRepository.test.ts
@@ -0,0 +1,25 @@
+import {
+  Field,
+  Mina,
+  PrivateKey,
+  PublicKey,
+  AccountUpdate,
+  CircuitString,
+  Encryption,
+  Encoding,
+} from 'o1js';
+import * as fs from 'fs';
+import { CredentialRepository } from '../CredentialRepository';
+import { CredentialMetadata } from '../CredentialMetadata';
+
+let repo = new CredentialRepository();
+let credential = new CredentialMetadata(
+  'Test',
+  'Test',
+  '1.0',
+  new Date(),
+  'Test',
+  'Test',
+  'Test'
+);
+await repo.AddCredential(credential);
