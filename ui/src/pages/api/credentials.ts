3249a9a654d268aba32820e7301c58f2235d025c
KerishaStewart <62780934+KerishaStewart@users.noreply.github.com>
2023-10-23 23:26:40 -0400

    credentials api updated, 1 endpoint method, not 3, credential generation code inserted into api

--- ui/src/pages/api/credentials.ts
+++ ui/src/pages/api/credentials.ts
@@ -1,43 +1,54 @@
 // pages/api/credentials.ts
 
 import { NextApiRequest, NextApiResponse } from 'next';
+import CredentialGenerator from '../../../../contracts/src/CredentialGenerator';
 
 import * as fs from 'fs';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-    const { method, query } = req;
-
-    switch (method) {
-        case 'POST':
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if (req.method === 'POST') {
+        try {
+            const { method, query } = req;
+            // 1. Generate the credential file based on the JSON configuration.
             const { json, user } = req.body;
-            GenerateCredentialFile(json);
-            res.status(200).json({ message: 'Credential file generated successfully' });
-            break;
-        case 'PUT':
+            const generatedCredential = GenerateCredentialFile(json);
+
+            // 2. Deploy the credential file (replace with your deployment logic).
             const { filename } = query;
-            DeployCredential(filename);
-            res.status(200).json({ message: `Credential file '${filename}' deployed successfully` });
-            break;
-        case 'PATCH':
+            const deploymentResult = DeployCredential(filename);
+
+            // 3. Save metadata to Firebase database (replace with your Firebase SDK usage).
             const { credentialName, user: metadataUser } = req.body;
-            AddFirebaseMetadata(credentialName, metadataUser);
-            res.status(200).json({ message: `Firebase metadata added for '${credentialName}'` });
-            break;
-        default:
-            res.status(405).end(); // Method Not Allowed
+            const firebaseMetadataResult = AddFirebaseMetadata(credentialName, metadataUser);
+
+            res.status(200).json({
+                message: 'Credential file generated, deployed, and metadata saved successfully',
+                deploymentResult,
+                firebaseMetadataResult,
+            });
+        } catch (error: any) {
+            res.status(500).json({ error: 'Internal server error', details: error.message });
+        }
+    } else {
+        res.status(405).json({ error: 'Method not allowed' });
     }
 };
 
-function GenerateCredentialFile(json: any) {
+function GenerateCredentialFile(json: any): string {
     // Implement this method to generate the credential file based on the JSON configuration.
     // You can use the fs module to write the file to a specific location.
     // Example: fs.writeFileSync('path/to/credential/file.ts', generatedContent);
 
-    // ToDo
     // Access the CredentialGenerator file in the contracts project
     // It exposes a `generateAndSave` method
     // Give it a json string and a file path as params to generate creds
     // The json string has the fields for the credentials, the file path is where the template for the Credential generation is located
+    const template = "";
+    const generator = new CredentialGenerator();
+    generator.generateAndSave(json, template);
+
+
+    return "Credential generated"
 }
 
 function DeployCredential(filename: string) {
