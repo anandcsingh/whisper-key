9bd5c742fb3e818b74f9b6a03b68ffc62c98e429
pteelucksingh <peterakasalt@yahoo.com>
2023-10-25 23:56:24 -0400

    Issue #24 resolved

--- ui/src/pages/index.page.tsx
+++ ui/src/pages/index.page.tsx
@@ -23,10 +23,10 @@ export default function Home() {
           <section className=" dark:bg-gray-900">
             <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
               <div className="mr-auto place-self-center lg:col-span-7">
-                <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">Trusted Rank <br />Approval</h1>
-                <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">With Mina’s blockchain and Zero Knowledge proofs our platform will verify and share belt ranking, promote students to new ranks, even prove martial arts lineage and show it visually..<a href="https://flowbite.com/docs/getting-started/introduction/" className="hover:underline">Flowbite Library</a> and the <a href="https://flowbite.com/blocks/" className="hover:underline">Blocks System</a>.</p>
+                <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">Privacy enhanced Verifiable Credentials (VC) Hub</h1>
+                <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">A platform where Verifiable Credential Issuers can create new types of credentials or reuse existing standards and issue them to owners with ease. Owners selectively disclose some or all the data in their Verifiable Credential with Verifiers via Zero Knowledge proofs while Verifiers obtain only the information needed. <a href="https://flowbite.com/docs/getting-started/introduction/" className="hover:underline">Flowbite Library</a> and the <a href="https://flowbite.com/blocks/" className="hover:underline">Blocks System</a>.</p>
                 <div className="space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
-                  <a href="https://github.com/anandcsingh/rankproof" className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                  <a href="https://github.com/anandcsingh/whisper-key" className="inline-flex items-center justify-center w-full px-5 py-3 text-sm font-medium text-center text-gray-900 border border-gray-200 rounded-lg sm:w-auto hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                     <svg className="w-4 h-4 mr-2 text-gray-500 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">{/* Font Awesome Pro 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) */}<path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" /></svg> View on GitHub
                   </a> 
 
@@ -52,27 +52,27 @@ export default function Home() {
               {/* Row */}
               <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
                 <div className="text-gray-500 sm:text-lg dark:text-gray-400">
-                  <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">A web-based platform for every Martial Artists of any discipline</h2>
-                  <p className="mb-8 font-light lg:text-xl">All Martial Artists can view their lineage going as far back as we have data for practitioners. All powered by the Mina blockchain and Zero-Knowledge Proofs.</p>
+                  <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">A web-based platform for creating Verifiable Credentials</h2>
+                  <p className="mb-8 font-light lg:text-xl"> Below are some of the features of the platform, all powered by the Mina blockchain and Zero-Knowledge Proofs:</p>
                   {/* List */}
                   <ul role="list" className="pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700">
                     <li className="flex space-x-3">
                       {/* Icon */}
                       <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
-                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Create a rank in various martial arts</span>
+                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">As an Issuer based on user input automatically generate the definition of a credential and the supporting smart contract to create its proof</span>
                     </li>
                     <li className="flex space-x-3">
                       {/* Icon */}
                       <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
-                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Web-based zkApp portal</span>
+                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Search for existing credential types</span>
                     </li>
                     <li className="flex space-x-3">
                       {/* Icon */}
                       <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
-                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Promote/verify/revoke their students’ belt rank</span>
+                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Issue a Verifiable Credential to an Owner</span>
                     </li>
                   </ul>
-                  <p className="mb-8 font-light lg:text-xl">Instructors have control over their students&#39; rank, being able to promote or revoke their rank.</p>
+                  {/* <p className="mb-8 font-light lg:text-xl">Instructors have control over their students&#39; rank, being able to promote or revoke their rank.</p> */}
                 </div>
                 <img className="hidden w-full mb-4 rounded-lg lg:mb-0 lg:flex" src="/assets/images/pngwingcom.png" alt="dashboard feature image" />
               </div>
@@ -80,19 +80,19 @@ export default function Home() {
               <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
                 <img className="hidden w-full mb-4 rounded-lg lg:mb-0 lg:flex" src="/assets/images/jutsu.png" alt="feature image 2" />
                 <div className="text-gray-500 sm:text-lg dark:text-gray-400">
-                  <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">All Martial Artists can view their lineage</h2>
-                  <p className="mb-8 font-light lg:text-xl">As a martial artist discover and navigate your history through a beautiful interactive visualization of your lineage.</p>
+                  <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">All users can view their verified credentials</h2>
+                  <p className="mb-8 font-light lg:text-xl">Users have access to all verifiable credentials issued to them.</p>
                   {/* List */}
                   <ul role="list" className="pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700">
                     <li className="flex space-x-3">
                       {/* Icon */}
                       <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
-                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Share your rank via QR Code</span>
+                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Share your verifiable credential via QR Code</span>
                     </li>
                     <li className="flex space-x-3">
                       {/* Icon */}
                       <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
-                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">View lineage</span>
+                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">View all credentials</span>
                     </li>
                     <li className="flex space-x-3">
                       {/* Icon */}
@@ -102,15 +102,15 @@ export default function Home() {
                     <li className="flex space-x-3">
                       {/* Icon */}
                       <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
-                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">RankProof zkApp UI</span>
+                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Whisper Key zkApp UI</span>
                     </li>
-                    <li className="flex space-x-3">
+                    {/* <li className="flex space-x-3"> */}
                       {/* Icon */}
-                      <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg>
-                      <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Verify Smart Contract</span>
-                    </li>
+                      {/* <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" /></svg> */}
+                      {/* <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Verify Smart Contract</span> */}
+                    {/* </li> */}
                   </ul>
-                  <p className="font-light lg:text-xl">Written in TypeScript using the o1js library will run on the browser client generating a proof of student rank verification by a certified instructor. </p>
+                  <p className="font-light lg:text-xl">Written in TypeScript using the o1js library will run on the browser client generating a verifiable credential that will be issued by a certified issuer. </p>
                 </div>
               </div>
             </div>
@@ -122,7 +122,7 @@ export default function Home() {
             <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-16 lg:px-6">
               <div className="max-w-screen-sm mx-auto text-center">
                 <h2 className="mb-4 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">Start your free trial today</h2>
-                <p className="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">As a martial artist discover and navigate your history thru a beautiful interactive visualization of your lineage. </p>
+                <p className="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">Enjoy using our platform and have data sovereignty with verifiable credentials issued as zk proofs by trusted parties using zk oracles </p>
                 <a href="#" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Free trial for 30 days</a>
               </div>
             </div>
@@ -135,7 +135,7 @@ export default function Home() {
               <div className="text-center">
                 <a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                   <img src="/assets/images/logo.png" className="h-6 mr-3 sm:h-9" alt="RankProof Logo" />
-                  RankProof    
+                  Whisper Key    
                 </a>
                 <span className="block text-sm text-center text-gray-500 dark:text-gray-400">© 2021-2022 RankProof™. All Rights Reserved.
                 </span>
