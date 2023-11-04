bda5d3d548c50965676cfb5c4802009f46fded62
anandcsingh <anandcsingh@gmail.com>
2023-11-04 16:19:07 -0400

    dirs hello

--- ui/src/pages/api/hello.ts
+++ ui/src/pages/api/hello.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
 type Data = {
   name: string
 }
@@ -9,5 +11,23 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  console.log("home directory:" + getUserHome()); 
+  console.log("temp directory:" + os.tmpdir()); 
+  var dir = path.resolve(os.tmpdir(), 'credentials');
+  console.log("temp directory:" + dir);
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir, { recursive: true });
+  }
+  fs.writeFileSync(path.resolve(dir, 'test.txt'), 'test');
+  const result = fs.readFileSync(path.resolve(dir, 'test.txt'), 'utf-8');
+
+  res.status(200).json({ name: result });
 }
+  
+function getUserHome() { 
+      
+    // From process.env 
+    return process.env[(process.platform == 'win32') 
+            ? 'USERPROFILE' : 'HOME']; 
+} 
