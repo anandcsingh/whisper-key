f6290f567256e92a72b115efda133204cd7ef4df
KerishaStewart <62780934+KerishaStewart@users.noreply.github.com>
2023-11-29 21:16:32 -0400

    view credential modal info added

--- ui/src/components/sections/DashboardCredentialsCreated.tsx
+++ ui/src/components/sections/DashboardCredentialsCreated.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState, useContext } from "react";
 import { AuthContext } from '@/components/layout/AuthPage';
 import { CredentialMetadata, CredentialField } from '../../modules/CredentialMetadata';
 import axios from "axios";
+import Link from 'next/link'
 
 const DashboardCredentialsCreated = () => {
 
@@ -49,11 +50,35 @@ const DashboardCredentialsCreated = () => {
             ) : (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     {created.credentials.map((credential: any, index: number) => (
-                        <div key={index} className="bg-white p-4 shadow-lg rounded-md">
-                            <h3 className="text-xl font-semibold">{credential.credentialType}</h3>
-                            <p className="text-gray-600">{credential.description}</p>
-                            <button className="btn btn-sm btn-primary">View Credential</button>
-                        </div>
+                       <div key={index}>
+                            <div className="bg-white p-4 shadow-lg rounded-md">
+                                <h3 className="text-xl font-semibold">{credential.credentialType}</h3>
+                                <p className="text-gray-600">{credential.description}</p>
+                                <a href={`#${credential.description}_modal`} className="btn btn-sm btn-primary">View Credential</a>
+                            </div>
+                            <div className='modals-area'>
+                            <dialog className="modal" id={`${credential.description}_modal`}>
+                                <form method="dialog" className="modal-box w-11/12 max-w-5xl">
+                                    <div className="modal-action">
+                                        <a href="#" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">X</a>
+                                    </div>
+                                    <h1>Credential Name: {credential.name}</h1><br />
+                                    <div>Owner: {credential.owner}</div>
+                                    <div>Description: {credential.description}</div> <br />
+                                    <h2>Fields</h2>
+                                    {credential.fields.map((field: CredentialField, index: number) => {
+                                        <div className='data-field' key={index}>
+                                            <div>Name: {field.name}</div>
+                                            <div>Type: {field.type}</div>
+                                        </div>
+                                    })}
+                                </form>
+                                <form method="dialog" className="modal-backdrop">
+                                    <button>close</button>
+                                </form>
+                            </dialog>
+                            </div>
+                       </div>
                     ))}
                 </div>
             )}
