d73ff943e57dc25b8b9b6046d7a1ea8bf5c48467
KerishaStewart <62780934+KerishaStewart@users.noreply.github.com>
2024-01-22 23:34:34 -0400

    email functionality added to api

--- api/src/routes/messagingRoute.ts
+++ api/src/routes/messagingRoute.ts
@@ -1,8 +1,9 @@
 // routes/messagingRoute.ts
 import express, { Router } from 'express';
-import { sendSms, sendWhatsapp } from '../controllers/messagingController.js';
+import { sendEmail, sendSms, sendWhatsapp } from '../controllers/messagingController.js';
 
 export const messagingRouter = express.Router();
 
 messagingRouter.post('/sms', sendSms);
-messagingRouter.post('/whatsapp', sendWhatsapp)
+messagingRouter.post('/whatsapp', sendWhatsapp);
+messagingRouter.post('email', sendEmail);
