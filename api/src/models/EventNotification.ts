02b896b1b5ed40476681e82ce11a30145f8a2f42
anandcsingh <anandcsingh@gmail.com>
2024-01-22 22:47:03 -0400

    Created Credential Completed Check

--- api/src/models/EventNotification.ts
+++ api/src/models/EventNotification.ts
@@ -1,10 +1,19 @@
-import { CredentialRepository } from "contract-is-key";
+import { CredentialMetadata, CredentialRepository } from "contract-is-key";
 import { CronJob } from "node-cron";
 import { PublicKey, UInt32 } from "o1js";
 
 export class EventNotification {
+
+    async pushCreated(cred: CredentialMetadata) {
+        
+        // store in firebase for web notifications
+
+        // send omni-channel notifications
+
+    }
+
     
-    async push(name: string, events: any[]) {
+    async pushIssued(name: string, events: any[]) {
         
         // store in firebase for web notifications
 
