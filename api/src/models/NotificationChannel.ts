a19a16deb269a6db291cc9ea4fddb173842323cb
anandcsingh <anandcsingh@gmail.com>
2024-01-27 16:22:26 -0400

    Notifications factory

--- api/src/models/NotificationChannel.ts
+++ api/src/models/NotificationChannel.ts
@@ -1,26 +1,11 @@
-import { EmailChannel } from './EmailChannel';
-import { MessageDestination } from './MessageDestination';
-import { SmsChannel } from './SmsChannel';
-import { WhatsAppChannel } from './WhatsAppChannel';
-export class NotificationChannel {
+
+import { MessageDestination } from './MessageDestination.js';
+
+export abstract class NotificationChannel {
     channel: string;
 
     constructor(channel: string) {
         this.channel = channel;
-    }
-
-    static createChannel(channel: string): NotificationChannel {
-        switch (channel) {
-            case 'email':
-                return new EmailChannel(channel);
-            case 'sms':
-                return new SmsChannel(channel);
-            case 'whatsapp':
-                return new WhatsAppChannel(channel);
-            // Add more cases for other channel types
-            default:
-                throw new Error(`Unsupported channel: ${channel}`);
-        }
     }
 
     sendMessage(destination: MessageDestination, message: string): void { }
