ba6b185ab083dce134bb145ae73111c34e40a877
KerishaStewart <62780934+KerishaStewart@users.noreply.github.com>
2024-03-17 23:55:16 -0400

    feat: sending to smart contract and sending to another address methods

--- whisper-key-core/src/Escrow.ts
+++ whisper-key-core/src/Escrow.ts
@@ -1,10 +1,11 @@
-import { SmartContract, method, UInt64, AccountUpdate, PublicKey } from 'o1js';
+import { Mina, SmartContract, method, UInt64, AccountUpdate, PublicKey } from 'o1js';
 
 export class Escrow extends SmartContract {
     init(): void {
 
     }
 
+    // deposit to sender account from smart contract
     @method async deposit(user: PublicKey) {
         // add your deposit logic circuit here
         // that will adjust the amount
@@ -14,4 +15,21 @@ export class Escrow extends SmartContract {
 
         payerUpdate.send({ to: this.address, amount: UInt64.from(1000000) });
     }
+
+    // deposit to smart contract from sender account
+    @method depositToSmartContract(amount: UInt64) {
+        let senderUpdate = AccountUpdate.create(this.sender);
+        senderUpdate.requireSignature();
+        console.log('Smart contract balance before:')
+        printBalances(this.address);
+        senderUpdate.send({ to: this, amount });
+        console.log('Smart contract balance after:');
+        printBalances(this.address);
+    }
+}
+
+function printBalances(addr: PublicKey) {
+    console.log(
+        `zkApp balance:  ${Mina.getBalance(addr).div(1e9)} MINA`
+    );
 }
