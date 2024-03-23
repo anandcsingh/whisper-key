ce435f99d109007b609946f958a0a204efff30a0
KerishaStewart <62780934+KerishaStewart@users.noreply.github.com>
2024-03-22 23:15:04 -0400

    wip escrow data forming and managing

--- api/src/routes/escrowPaymentRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getEscrowPaymentData, addEscrowPaymentData } from "../controllers/escrowController";
-
-export const escrowRouter = express.Router();
-
-//Routes
-escrowRouter.get('/', getEscrowPaymentData);
-escrowRouter.post('/', addEscrowPaymentData);
