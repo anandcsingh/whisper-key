b527eae8e7fe1cd02e73f7fab3e202a2cd6f432b
KerishaStewart <62780934+KerishaStewart@users.noreply.github.com>
2024-03-27 23:32:30 -0400

    feat: escrow smart contract per user deployment working

---
+++ smartcontract-deployer/src/app.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from 'express';
+import { EscrowBerkeleyDeployer } from './EscrowBerkeleyDeployer.js';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Example GET endpoint
+app.get('/api/', (req: Request, res: Response) => {
+    // Logic to handle GET request
+    res.json({ message: 'GET request received' });
+});
+
+app.post('/api/deploy', async (req: Request, res: Response) => {
+    const { senderAccount, receiverAccount } = req.body;
+
+    if (!senderAccount || !receiverAccount) {
+        return res.status(400).json({ error: 'senderAccount and receiverAccount are required in the request body' });
+    }
+
+    let smartContractPublicKey = "abc1234567";
+    // let deployer = new EscrowBerkeleyDeployer(senderAccount, receiverAccount);
+    // let smartContractPublicKey = await deployer.deploy();
+    // console.log(smartContractPublicKey);
+
+    if (smartContractPublicKey)
+        res.json({ message: 'Deploy initiation successful', smartContractPublicKey, senderAccount, receiverAccount });
+    else
+        res.json({ message: 'Deploy initiation not successful', smartContractPublicKey, senderAccount, receiverAccount });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
