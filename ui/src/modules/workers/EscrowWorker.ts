9b4e0b691a090d2597d108a4eef2a24aa9fa4c71
KerishaStewart <62780934+KerishaStewart@users.noreply.github.com>
2024-03-31 23:26:43 -0400

    chore: minor deployer and UI updates

--- ui/src/modules/workers/EscrowWorker.ts
+++ ui/src/modules/workers/EscrowWorker.ts
@@ -4,7 +4,7 @@ type Transaction = Awaited<ReturnType<typeof Mina.transaction>>;
 
 // ---------------------------------------------------------------------------------------
 
-import type { EscrowContract } from '../contracts/EscrowContract';
+import type { EscrowContract } from '../../../../smartcontract-deployer/build/src/EscrowContract.js';
 
 const state = {
     EscrowContract: null as null | typeof EscrowContract,
@@ -16,14 +16,17 @@ const state = {
 
 const functions = {
     setActiveInstanceToBerkeley: async (args: {}) => {
+        console.log('setting network instance');
         const Berkeley = Mina.Network(
-            'https://api.minascan.io/node/berkeley/v1/graphql'
+            //'https://api.minascan.io/node/berkeley/v1/graphql'
+            //'https://berkeley.graphql.minaexplorer.com/'
+            'https://proxy.berkeley.minaexplorer.com/graphql'
         );
         console.log('Berkeley Instance Created');
         Mina.setActiveInstance(Berkeley);
     },
     loadContract: async (args: {}) => {
-        const { EscrowContract } = await import('../../../../contracts/EscrowContract.js');
+        const { EscrowContract } = await import('../../../../smartcontract-deployer/build/src/EscrowContract.js');
         state.EscrowContract = EscrowContract;
     },
     compileContract: async (args: {}) => {
@@ -41,8 +44,10 @@ const functions = {
         const currentNum = await state.zkapp!.escrowAmount.get();
         return JSON.stringify(currentNum.toJSON());
     },
-    depositToSmartContract: async (args: {}) => {
-        const transaction = await Mina.transaction(() => {
+    depositToSmartContract: async (args: { publicKey: string }) => {
+        let fee = 100_000_000;
+        let sender = PublicKey.fromBase58(args.publicKey);
+        const transaction = await Mina.transaction({ sender, fee }, () => {
             state.zkapp!.deposit(UInt64.from(2 * 1e9));
         });
         state.transaction = transaction;
